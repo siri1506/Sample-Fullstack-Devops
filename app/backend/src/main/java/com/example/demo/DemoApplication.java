0da21d7032b1a8e3461a62c2a06e249f41ab964f
siri1506 <srihithasiri1506@gmail.com>
2025-11-14 14:55:30 -0500

    added backend and front end app structure

---
+++ app/backend/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+  public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args); }
+
+  @GetMapping("/api/hello")
+  public String hello() { return "Hello from Spring Boot API"; }
+}
